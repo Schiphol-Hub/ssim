60234960cd813530959aaebc05b01724f26f0289
Rok Mihevc <rok@mihevc.org>
2017-03-30 01:43:44 +0200

    naming issue

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'SSIM',
   packages = ['SSIM'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'A parser for SSIM',
   author = 'Rok Mihevc',
   author_email = 'rok.mihevc@gmail.com',
   url = 'https://github.com/rok/SSIM',
-  download_url = 'https://github.com/rok/SSIM/archive/0.1.tar.gz',
+  download_url = 'https://github.com/rok/SSIM/archive/0.1.1.tar.gz',
   keywords = ['parsing', 'SSIM', 'data'],
   classifiers = [],
 )
