5850c385016217a64079187cac07cb69583eccb5
Rok <rok@mihevc.org>
2018-01-17 22:58:55 +0100

    adding sim test cases

---
+++ test/test_parsing.py
@@ -0,0 +1,12 @@
+from ssim.ssim2 import _parse_sir, _parse_sim
+
+
+def test_sir_parsing(sir_records):
+
+    for record in sir_records:
+        assert _parse_sir(record['raw_data']) == record['records']
+
+def test_sim_parsing(sim_records):
+
+    for record in sim_records:
+        assert _parse_sim(record['raw_data']) == record['records']
